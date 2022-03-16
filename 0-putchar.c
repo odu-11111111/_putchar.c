fbf3216563b7f9ad49066adebf852301f739ba86
Olayinka Onawola <OlayinkaOnawola@gmail.com>
2022-03-16 05:59:01 +0000

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,19 @@
+int _putchar(char c);
+/**
+ * main - function to print char to stdout
+ * @c: The character to print
+ * Return: 0
+ */
+int main()
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
